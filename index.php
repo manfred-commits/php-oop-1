ca3d92aa2afb5edd9c42062d20a7bf001971bc2e
manfred-commits <manfredipiraino@gmail.com>
2021-11-02 15:35:34 +0100

    added comment fix 'fine esercizio'

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
-<!-- Oggi pomeriggio ripassate i primi concetti di classe e di variabili e
-metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
-- è definita una classe ‘Movie’
-   => all'interno della classe sono dichiarate delle variabili d'istanza
-   => all'interno della classe è definito un costruttore
-   => all'interno della classe è definito almeno un metodo
-- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà -->
-
-
 <?php
+
+// Oggi pomeriggio ripassate i primi concetti di classe e di variabili e
+// metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
+// - è definita una classe ‘Movie’
+//    => all'interno della classe sono dichiarate delle variabili d'istanza
+//    => all'interno della classe è definito un costruttore
+//    => all'interno della classe è definito almeno un metodo
+// - vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
+
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
